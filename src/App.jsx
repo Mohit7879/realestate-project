08255f8a2e2b1aa5e3a96a0f659a9865594accbe
mohit <mohityadavkkn24@gmail.com>
2023-11-04 18:14:40 +0530

    routes created

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,25 @@
+
+import {BrowserRouter , Routes, Route } from 'react-router-dom';
+import Home from './pages/Home';
+import Profile from './pages/Profile';
+import Signin from './pages/Signin';
+import SignOut from './pages/SignOut';
+
+
 export default function App() {
   return (
-    <h1 className="text-red-500">
-      App
-    </h1>
+    <>
+    
+    <BrowserRouter>
+    <Routes>
+      <Route path="/" element={<Home/>} />
+      <Route path="/profile" element={<Profile/>} />
+      <Route path="/signin" element={<Signin/>} />
+      <Route path="/signout" element={<SignOut/>} />
+     
+    </Routes>
+  </BrowserRouter>
+  </>
+  
   )
 }
---
+++ src/pages/About.jsx
@@ -0,0 +1,9 @@
+
+
+export default function About() {
+  return (
+    <div className="text-red-500">
+      About
+    </div>
+  )
+}
